21d8e310b9c7cae702818dd82d902784eb1df777
milocode-dev <radityaakmal277@gmail.com>
2026-06-30 19:22:12 +0700

    Mereturn halaman halo dari method index

--- app/Http/Controllers/HaloController.php
+++ app/Http/Controllers/HaloController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HaloController extends Controller
 {
     public function index() {
-        return "Halo dari Controller!";
+        return view('halo');
     }
 }
